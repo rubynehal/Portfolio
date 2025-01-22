bebc15ac52e9aca03c0aee67169f4bf40f95d43e
nehal jaiswal <nehaljais5@gmail.com>
2025-01-22 16:46:16 +0530

    Normalize line endings

---
+++ src/assets/index.js
@@ -0,0 +1,94 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import pyspark from "./tech/pyspark.png";
+import azure from "./tech/azure.png";
+import python from "./tech/python.png";
+import sql from "./tech/sql.png";
+import aws from "./tech/aws.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import lt from "./company/lt.png";
+import riversilica from "./company/riversilica.jpg";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import portfolio from "./portfolio.png";
+import aviation from "./aviation.jpg";
+import disk_mirroring from "./disk_mirroring.webp";
+import cheelgaadi from "./cheelgaadi.png";
+import scte from "./scte.webp";
+import license from "./license.jpg";
+import tomato from "./tomato.jpeg";
+import iot from "./iot.jpeg";
+import plant from "./plant.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  lt,
+  riversilica,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  pyspark,
+  python,
+  sql,
+  azure,
+  aws,
+  portfolio,
+  aviation,
+  disk_mirroring,
+  cheelgaadi,
+  scte,
+  license,
+  tomato,
+  iot,
+  plant
+};
---
+++ src/constants/index.js
@@ -0,0 +1,482 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    lt,
+    riversilica,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    pyspark,
+    azure,
+    python,
+    sql,
+    aws,
+    portfolio,
+    aviation,
+    disk_mirroring,
+    cheelgaadi,
+    scte,
+    license,
+    tomato,
+    iot,
+    plant
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Full Stack Developer Intern",
+      icon: web,
+    },
+    {
+      title: "Software Engineer",
+      icon: mobile,
+    },
+    {
+      title: "Graduate Engineering Trainee",
+      icon: backend,
+    },
+    {
+      title: "Data Engineer",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "Python",
+      icon: python,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Pyspark",
+      icon: pyspark,
+    },
+    {
+      name: "SQL",
+      icon: sql,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "Azure",
+      icon: azure,
+    },
+    {
+      name: "AWS",
+      icon: aws,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Full Stack Developer Intern",
+      company_name: "Riversilica Technologies Pvt Ltd",
+      icon: riversilica,
+      iconBg: "#383E56",
+      date: "April 2023 - September 2023",
+      points: [
+        "Migrating legacy applications from Angular to the MERN Stack, enhancing performance and maintainability.",
+        "Designing and implementing new frontend pages and options to improve user experience and functionality.",
+        "Developing features to monitor and manage access through license creation, enabling media device and service usage restrictions.",
+        "Facilitating user management by integrating options for adding users and licenses based on various types and encryption methods.",
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including CTO, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing feedbacks to other developers.",
+      ],
+    },
+    {
+      title: "Software Engineer",
+      company_name: "Riversilica Technologies Pvt Ltd",
+      icon: riversilica,
+      iconBg: "#E6DEDD",
+      date: "October 2023 - August 2024",
+      points: [
+        "Developed and maintained user interfaces for the Disk Mirroring User Interface, enhancing the experience for adding disk mirroring schedules.",
+        "Implemented JSON-based export and import functionalities, enabling users to easily transfer disk mirroring configurations across systems.",
+        "Engineered intuitive Start and Stop buttons for seamless control over scheduling, including validations to prevent duplicate entries.",
+        "Collaborated with cross-functional teams to ensure the timely delivery of features while maintaining high-quality standards.",
+        "Built a comprehensive application for the LBAND, PUSHUP & SCTE Insertion, allowing users to schedule and manage content insertion effectively.",
+        "Monitored active schedules and provided real-time updates on currently displayed SCTE, PUSHUP, and LBAND elements, improving user engagement.",
+        "Designed and implemented features allowing users to add, delete, edit, and copy schedules with ease, enhancing overall usability.",
+        "Participated in code reviews, fostering collaboration and providing constructive feedback to team members to optimize code quality."
+      ],
+    },
+    {
+      title: "Graduate Engineer Trainee",
+      company_name: "LTIMindtree",
+      icon: lt,
+      iconBg: "#383E56",
+      date: "August 2024 - November 2024",
+      points: [
+        "Got trained in Python, SQL, Azure, Power BI, and Communication. Gained foundational knowledge and hands-on experience in these tools, enhancing problem-solving and teamwork.",
+        "Developed ETL Pipeline Using Azure Data Factory. Designed and implemented workflows to extract, transform, and load data for data-driven decision-making.",
+        "Optimized Data Architecture with Medallion Approach. Improved data quality and reusability using the Medallion Architecture for processing and storage.",
+        "Managed Large Datasets with Azure Blob Storage and ADLS: Utilized Azure Blob Storage and ADLS for scalable and secure data storage.",
+        "Data Cleaning, Profiling, and Validation with Azure Databricks: Refined raw data and ensured readiness for analysis using Azure Databricks.",
+        "Created Data Visualizations with Power BI: Designed interactive dashboards to present insights into aviation delays and cancellations.",
+        "Collaborated on Data Engineering Project: Contributed to the 'Optimizing Aviation Insights' project for analyzing flight delays and cancellations.",
+        "Enhanced Communication and Collaboration Skills: Participated in discussions and knowledge-sharing sessions to foster teamwork."
+      ],
+    },
+    {
+      title: "Data Engineer",
+      company_name: "LTIMindtree",
+      icon: lt,
+      iconBg: "#E6DEDD",
+      date: "November 2024 - Present",
+      points: [
+        "Working with PySpark for Big Data Processing. Utilizing PySpark to efficiently handle and process large-scale datasets, ensuring high-performance data pipelines.",
+        "Advanced SQL Development. Writing complex SQL queries for data extraction, transformation, and analysis to support business objectives.",
+        "Building Scalable Data Solutions. Designing and optimizing data pipelines to handle large volumes of data while ensuring scalability and reliability.",
+        "Focused on Professional Certifications. Actively pursuing Azure and AWS certifications to enhance cloud expertise and professional credibility.",
+        "Collaborating with cross-functional teams including, product managers, and other developers to create high-quality products.",
+        "Participating in code reviews and providing constructive feedback to other developers."
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Portfolio",
+      description:
+        [ "[December 2024 - January 2025]",
+          "Developed a portfolio using React Three Fiber, Three.js, and React Drei to highlight 3D visuals and engaging animations.",
+          "Crafted a modern, user-friendly interface with Tailwind CSS, ensuring seamless responsiveness across devices.",
+          "Used Vite for efficient development and EmailJS to enable effortless communication through the contact form.",
+          "Designed this portfolio to creatively showcase my technical expertise and projects, demonstrating mastery of cutting-edge web technologies.",],
+      tags: [
+        {
+          name: "React.js",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "Three.js",
+          color: "teal-text-gradient",
+        },
+        {
+          name: "React Three Fiber",
+          color: "yellow-text-gradient",
+        },
+        {
+          name: "React Three Drei",
+          color: "red-text-gradient",
+        },
+        {
+          name: "Email JS",
+          color: "orange-text-gradient",
+        },
+        {
+          name: "Vite",
+          color: "green-text-gradient",
+        },
+      ],
+      image: portfolio,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Optimizing Aviation Insights: A Data Engineering Approach for Analyzing Flight Delays and Cancellations [Confidential Project]",
+      description:
+        [ "[October 2024 - November 2024]",
+          "Developed an ETL pipeline using Azure Data Factory",
+        "Used Medallion Architecture for ensuring data quality and reliability",
+        "Storing Large Datasets using Blob Storage and ADLS",
+        "Used Azure DataBricks for Data Cleaning, profiling and validation.",
+        "Made various transformations and Graphs with the help of PowerBI."],
+      tags: [
+        {
+          name: "Microsoft Azure",
+          color: "orange-text-gradient",
+        },
+        {
+          name: "Azure Data Factory",
+          color: "green-text-gradient",
+        },
+        {
+          name: " Azure Databricks",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "Azure Data Lake",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Microsoft Power BI",
+          color: "yellow-text-gradient",
+        },
+      ],
+      image: cheelgaadi,
+      source_code_link: "",
+    },
+    {
+      name: "Disk Mirroring User Interface [Confidential Project]",
+      description:
+        [ "[May 2024 - August 2024]",
+          "Worked on developing user interface for adding Disk Mirroring schedules.",
+          "Worked on developing a export and import feature with JSON file, so that disk mirroring with same configurations can be added in multiple systems.",
+          "Added Start and Stop button for starting and stopping multiple schedules by one click, added validations for duplicate schedules.",
+        ],
+      tags: [
+        {
+          name: "Disk Mirroring",
+          color: "gold-text-gradient",
+        },
+        {
+          name: "JavaScript",
+          color: "red-text-gradient",
+        },
+        {
+          name: "REST APIs",
+          color: "lime-text-gradient",
+        },
+      ],
+      image: disk_mirroring,
+      source_code_link: "",
+    },
+    {
+      name: "LBAND, PUSHUP & SCTE Insertion Application [Confidential Project]",
+      description:
+        [ "[December 2023 - April 2024]",
+          "This Project facilitates Insertion of SCTE, PUSHUP and LBAND where user can decide on the time, when the insertion has to be done.",
+          "It also keeps tab on which SCTE, PUSHUP or LBAND is currently being displayed, including its duration.",
+          "User can stop the SCTE, PUSHUP or LBAND manually or else it'll be stopped when time duration is over",
+          "User can add, delete, edit and copy the Schedules(LBAND/PUSHUP/SCTE).",
+          "User can see Schedule history, if a SCTE schedule is added while LBAND/PUSHUP schedule is running then the currently running schedule will pause and continue after SCTE schedule is over.",
+          "Worked on developing a export and import feature with JSON file, so that schedules with same configurations can be added in multiple systems.",
+        ],
+      tags: [
+        {
+          name: "React.js",
+          color: "yellow-text-gradient",
+        },
+        {
+          name: "Javascript",
+          color: "teal-text-gradient",
+        },
+        {
+          name: "MySQL",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "REST APIs",
+          color: "orange-text-gradient",
+        },
+      ],
+      image: scte,
+      source_code_link: "",
+    },
+    {
+      name: "License Generator Application [Confidential Project]",
+      description:
+        [ "[April 2023 - November 2023]",
+          "Migrated the legacy application from Angular to MERN Stack.",
+          "Created new frontend pages and options.",
+          "Primarily used to monitor and give access by creating Licenses to restrict usage of the media device and services.",
+          "Had options to add Users, Licenses(Based on different types and encryption).",
+        ],
+      tags: [
+        {
+          name: "MERN Stack",
+          color: "red-text-gradient",
+        },
+        {
+          name: "HTML",
+          color: "yellow-text-gradient",
+        },
+        {
+          name: "Cascading Style Sheets(CSS)",
+          color: "gold-text-gradient",
+        },
+        {
+          name: "Javascript",
+          color: "lime-text-gradient",
+        },
+      ],
+      image: license,
+      source_code_link: "",
+    },
+    {
+      name: "Plant Leaf Disease Prediction",
+      description:
+        [ "[March 2023 - October 2023]",
+          "This Project facilitates accurate disease detection through application of ML algorithm such as CNN.",
+          "Have features like User Registration, User Login, leaf disease prediction,disease treatment suggestion and description, Leaf disease percentage prediction, chatbot.",
+          "Tools used - Flask, OpenCV, Javascript, Python, MY SQL.",
+          "Tested 3 models using - MobileNetV2, ConvNet and InceptionV3 algorithm. MobileNetV2 attained highest accuracy and is used to predict diseases in plants.",
+        ],
+      tags: [
+        {
+          name: "Flask",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "Javascript",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Python",
+          color: "gold-text-gradient",
+        },
+        {
+          name: "MySQL",
+          color: "red-text-gradient",
+        },
+        
+      ],
+      image: plant,
+      source_code_link: "https://github.com/rubynehal/Plant-Leaf-Disease-Prediction",
+    },
+    {
+      name: "Tomato Leaf Disease Prediction Using CNN",
+      description:
+        [ "[December 2022 - February 2023]",
+          "This project predicts disease in tomato leaf. There are 3 components.",
+          "User Interface - It handles front-end logic of application to upload image and getting prediction probability. Used Python, Flask, HTML & CSS.",
+          "Web Server - handles HTTP requests and returns response",
+          "Prediction Model - predict whether image is diseased or healthy based on image input. Used Alexnet algorithm, Tensorflow and Numpy."],
+      tags: [
+        {
+          name: "TensorFlow",
+          color: "yellow-text-gradient",
+        },
+        {
+          name: "Flask",
+          color: "red-text-gradient",
+        },
+        {
+          name: "Numpy",
+          color: "cyan-text-gradient",
+        },
+        {
+          name: "Python",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "lime-text-gradient",
+        },
+        {
+          name: "HTML",
+          color: "orange-text-gradient",
+        },
+      ],
+      image: tomato,
+      source_code_link: "https://github.com/rubynehal/Tomato-Leaf-Disease-Prediction-Using-CNN",
+    },
+    {
+      name: "Ambulance Rescue system with Health Monitoring (IOT based Project)",
+      description:
+        [ "[August 2022 - October 2022]",
+          "The main concept behind the project was to provide a smooth flow for the ambulance to reach the hospitals in time and thereby minimizing the delay caused by traffic congestion.", 
+          "The Microcontroller based RFID system was used to alter the traffic lights upon its arrival at traffic light junction which would save lives at critical time.",
+          "Ambulance sends emergency signal to traffic control to control the traffic pattern according to the traffic density and based on ambulance driver requirement.",],
+      tags: [
+        {
+          name: "Arduino",
+          color: "gold-text-gradient",
+        },
+        {
+          name: "Internet of Things(IOT)",
+          color: "lime-text-gradient",
+        },
+        {
+          name: "RFID",
+          color: "orange-text-gradient",
+        },
+      ],
+      image: iot,
+      source_code_link: "https://github.com/rubynehal/Ambulance-Rescue-system-with-Health-Monitoring-",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
